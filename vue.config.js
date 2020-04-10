cdd0f9304be0bd4965fa5ea41369edf916205b41
jackchen0120 <278910933@qq.com>
2020-04-10 16:56:29 +0800

    配置修改及代码优化

--- vue.config.js
+++ vue.config.js
@@ -30,22 +30,25 @@ module.exports = {
 			config.performance = {
 		      hints: 'warning', 
 		      maxAssetSize: 300000, // 整数类型（以字节为单位）
-		      maxEntrypointSize: 500000 // 整数类型（以字节为单位）
+		      maxEntrypointSize: 500000, // 整数类型（以字节为单位）
+          assetFilter: function(assetFilename) { // 只给出 js 文件的性能提示
+            return assetFilename.endsWith('.js');
+          }
 			}
 
-		config.plugins.push(
-      // 生产环境自动删除console
-      new UglifyJsPlugin({
-         	uglifyOptions: {
-          	compress: {
-            	drop_debugger: true,
-            	drop_console: true,
-          	},
-           	warnings: false
-         	},
-         	sourceMap: false,
-         	parallel: true,
-       })
+			config.plugins.push(
+	      // 生产环境自动删除console
+	      new UglifyJsPlugin({
+	         	uglifyOptions: {
+	          	compress: {
+	            	drop_debugger: true,
+	            	drop_console: true,
+	          	},
+	           	warnings: false
+	         	},
+	         	sourceMap: false,
+	         	parallel: true,
+	      })
       )
 		}
 

a3605058194d34aa90f2d916b5ea18d43b86d861
john <john>
2020-04-21 00:21:38 +0800

    update docs

--- src/assets/js/url.js
+++ src/assets/js/url.js
@@ -6,11 +6,11 @@ if (process.env.NODE_ENV == 'development') {
   //开发、测试环境
   // baseUrl = '/api' // mock默认地址
   // baseUrl = '//yapi.demo.qunar.com/mock/95397/api' // yapi配置生成服务器地址
-  baseUrl = 'http://api.xuandan.com/DataApi'
+  baseUrl = '//api.xuandan.com/DataApi'
 } else if (process.env.NODE_ENV == 'production') {
   console.log('prod')
   // 生产环境	
-  baseUrl = 'http://api.xuandan.com/DataApi'
+  baseUrl = '//api.xuandan.com/DataApi'
 }
   
 export {
